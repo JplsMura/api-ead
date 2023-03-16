6a3bd5e2a97f444917ab574de9a19ecbb7925f90
JplsMura <joaopedrolima.s@hotmail.com>
2023-03-16 18:19:08 -0300

    Get a single course by UUID

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Api\Course\{
 };
 use Illuminate\Support\Facades\Route;
 
+// Courses
 Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/', function () {
     return response()->json([
