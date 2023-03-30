07bfa90f5fc2ecc3af82d7532b1c218daf0c7da7
JplsMura <joaopedrolima.s@hotmail.com>
2023-03-29 21:13:48 -0300

    Test for Courses, creation of Trait that will assist in the development of tests

--- tests/Feature/Api/Auth/AuthTest.php
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -2,13 +2,13 @@
 
 namespace Tests\Feature\Api\Auth;
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\UtilsTrait;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use UtilsTrait;
+
     public function test_fail_auth()
     {
         $response = $this->postJson('/auth', []);
@@ -18,7 +18,7 @@ class AuthTest extends TestCase
 
     public function test_auth()
     {
-        $user = User::factory()->create();
+        $user = $this->createUser();
 
         $response = $this->postJson('/auth', [
             'email' => $user->email,
@@ -40,12 +40,10 @@ class AuthTest extends TestCase
 
     public function test_logout()
     {
-        $user = User::factory()->create();
-
-        $token = $user->createToken('test')->plainTextToken;
+        $token = $this->createTokenUser();
 
         $response = $this->postJson('/logout', [], [
-            'Authorization' => "Bearer {$token}"
+            'Authorization' => "Bearer {$token}",
         ]);
 
         $response->assertStatus(200);
@@ -60,12 +58,10 @@ class AuthTest extends TestCase
 
     public function test_me()
     {
-        $user = User::factory()->create();
-
-        $token = $user->createToken('test')->plainTextToken;
+        $token = $this->createTokenUser();
 
         $response = $this->getJson('/me', [
-            'Authorization' => "Bearer {$token}"
+            'Authorization' => "Bearer {$token}",
         ]);
 
         $response->assertStatus(200);
